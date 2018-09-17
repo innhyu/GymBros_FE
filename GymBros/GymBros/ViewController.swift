e9c9e6ddfed9424244cfe744c0a429feead68ed3
innhyu <innhyu@gmail.com>
2018-09-17 17:40:35 -0400

    Initial Swift App

---
+++ GymBros/GymBros/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GymBros
+//
+//  Created by 이인혁 on 17/09/2018.
+//  Copyright © 2018 Carnegie Mellon University IS Dept. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
