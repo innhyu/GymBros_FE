408c51fbdad486104bd911e1847cf666f1466e98
innhyu <innhyu@gmail.com>
2018-11-09 02:20:53 -0500

    WorkoutCreateView Controller is now somewhat working, with some validations as well

--- GymBros/GymBros/WorkoutCreateViewController.swift
+++ GymBros/GymBros/WorkoutCreateViewController.swift
@@ -8,11 +8,13 @@
 
 import UIKit
 import Alamofire
+import SwiftyJSON
 
 class WorkoutCreateViewController: UIViewController {
 
   @IBOutlet weak var workoutTitle: UITextField!
   @IBOutlet weak var workoutTime: UITextField!
+  @IBOutlet weak var workoutDuration: UITextField!
   @IBOutlet weak var workoutLocation: UITextField!
   @IBOutlet weak var workoutType: UITextField!
   @IBOutlet weak var teamSize: UITextField!
@@ -28,12 +30,39 @@ class WorkoutCreateViewController: UIViewController {
       // Dispose of any resources that can be recreated.
   }
   
-  @IBAction func createWorkout(){
+  @IBAction func createWorkout(_ sender: Any){
     let title = self.workoutTitle.text;
     let time = self.workoutTime.text;
+    let duration: Int? = Int(self.workoutDuration.text!);
     let location = self.workoutLocation.text;
     let type = self.workoutType.text;
-    let teamSize = self.teamSize.text;
+    let teamSize: Int? = Int(self.teamSize.text!);
+
+    let parameters: Parameters = [
+      "user_id": 1,
+      "title": title ?? nil,
+      "time": time, // "2007-12-04 00:00:00 -0000"
+      "duration": duration ?? nil,
+      "location": "WIEGAND GYMNASIUM",
+      "team_size": teamSize ?? nil
+    ]
+
+    Alamofire.request("https://cryptic-temple-10365.herokuapp.com/workouts", method: .post, parameters: parameters)
+    .validate(statusCode: 200..<300)
+    .responseJSON { response in
+      print("Request: \(String(describing: response.request))")   // original url request
+      print("Response: \(String(describing: response.response))") // http url response
+      print("Result: \(response.result)")                         // response serialization result
+
+      if let json = response.result.value {
+
+        print("JSON: \(json)") // serialized json response
+        let swiftyjson = JSON(json);
+        print(swiftyjson)
+        print(swiftyjson["location"])
+        self.performSegue(withIdentifier: "workoutCreated", sender: sender)
+      }
+    }
     
   }
   
