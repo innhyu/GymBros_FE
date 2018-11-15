db9b49681f74884db2ea84a3b9daba50ce3f068a
innhyu <innhyu@gmail.com>
2018-11-15 15:32:25 -0500

    Adding joining functionality

--- GymBros/GymBros/Workouts/WorkoutShowViewController.swift
+++ GymBros/GymBros/Workouts/WorkoutShowViewController.swift
@@ -12,42 +12,57 @@ import SwiftyJSON
 
 class WorkoutShowViewController: UIViewController {
   
-  @IBOutlet var name: UILabel!
-  @IBOutlet var time: UILabel!
-  @IBOutlet var location: UILabel!
-  @IBOutlet var type: UILabel!
-  @IBOutlet var size: UILabel!
-  var workout_id: Int?
+    @IBOutlet var name: UILabel!
+    @IBOutlet var time: UILabel!
+    @IBOutlet var location: UILabel!
+    @IBOutlet var type: UILabel!
+    @IBOutlet var size: UILabel!
+    @IBOutlet var workoutAction: UIButton!
+
+    var workout_id: Int?
+    var owner_id: Int = 0
+    var request = Request()
   
-  override func viewDidLoad() {
-    super.viewDidLoad()
-    
-    let formatter = DateFormatter()
-    
-    Alamofire.request("https://cryptic-temple-10365.herokuapp.com/workouts/\(workout_id!)").responseJSON { response in
-      print("Request: \(String(describing: response.request))")   // original url request
-      print("Response: \(String(describing: response.response))") // http url response
-      print("Result: \(response.result)")                         // response serialization result
-      
-      if let json = response.result.value {
+    override func viewDidLoad() {
+        super.viewDidLoad()
         
-        print("JSON: \(json)") // serialized json response
-        let swiftyjson = JSON(json)
-        self.name.text = swiftyjson["workout"]["title"].string
-        // Turning API date string into Date object
-        formatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.sssZ"
-        let date = formatter.date(from: swiftyjson["workout"]["time"].string!)
-        // Formatting date appropriately
-        formatter.dateFormat = "MMM dd, HH:mm"
-        self.time.text = formatter.string(from: date!)
+        request.loadUser()
         
-        self.location.text = swiftyjson["workout"]["location"].string
-        //        self.type.text = swiftyjson["workout"]["location"].string // There is no workout type yet;
-        self.size.text = String(swiftyjson["workout"]["team_size"].int!)
+        let formatter = DateFormatter()
         
-      }
-    }
-    
+        Alamofire.request("https://cryptic-temple-10365.herokuapp.com/workouts/\(workout_id!)").responseJSON { response in
+            print("Request: \(String(describing: response.request))")   // original url request
+            print("Response: \(String(describing: response.response))") // http url response
+            print("Result: \(response.result)")                         // response serialization result
+          
+            if let json = response.result.value {
+            
+                print("JSON: \(json)") // serialized json response
+                let swiftyjson = JSON(json)
+                self.name.text = swiftyjson["workout"]["title"].string
+                // Turning API date string into Date object
+                formatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.sssZ"
+                let date = formatter.date(from: swiftyjson["workout"]["time"].string!)
+                // Formatting date appropriately
+                formatter.dateFormat = "MMM dd, HH:mm"
+                self.time.text = formatter.string(from: date!)
+            
+                self.location.text = swiftyjson["workout"]["location"].string
+                //        self.type.text = swiftyjson["workout"]["location"].string // There is no workout type yet;
+                self.size.text = String(swiftyjson["workout"]["team_size"].int!)
+                
+                // Owner parsing section
+                self.owner_id = swiftyjson["owner"]["id"].int!
+            }
+        }
+        
+        if self.owner_id == request.user_id! {
+            workoutAction.setTitle("Finalize", for: .normal)
+        }
+        else {
+            workoutAction.setTitle("Accept", for: .normal)
+        }
+        
   }
   
 // Preparation is done in the table, not this
