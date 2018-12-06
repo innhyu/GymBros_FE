97fd4df3e33b407f1b6e663b86ecb3125b3f37ac
innhyu <innhyu@gmail.com>
2018-12-06 17:03:00 -0500

    Workout divided into different sections

--- GymBros/GymBros/Workouts/WorkoutsTableViewController.swift
+++ GymBros/GymBros/Workouts/WorkoutsTableViewController.swift
@@ -9,7 +9,8 @@ import SwiftyJSON
 
 class WorkoutsTableViewController: UITableViewController {
     // Saving workout title, time, location, and id
-    var workouts = [Workout]()
+    var workouts = [[Workout]]()
+    var sectionTitles = ["My Workouts", "All Workouts"]
     var request = Request()
   
     override func viewDidLoad() {
@@ -30,27 +31,34 @@ class WorkoutsTableViewController: UITableViewController {
               print("JSON: \(json)") // serialized json response
               let swiftyjson = JSON(json)
               
-              if !swiftyjson.isEmpty {
-                // Construct workout object for each
-                for swiftyjson in swiftyjson.arrayValue {
-                  let workout = Workout(swiftyjson: ["workout": swiftyjson])
-                    self.workouts.append(workout)
-                }
-                
-                self.tableView.reloadData()
-                
-              }
-            }
+              let myWorkouts = swiftyjson["user_workouts"]
+              let otherWorkouts = swiftyjson["other_workouts"]
+          
+              self.workouts.append(self.populateWorkouts(swiftyjson: myWorkouts))
+              self.workouts.append(self.populateWorkouts(swiftyjson: otherWorkouts))
+              
+              self.tableView.reloadData()
+
+          
+          }
         }
     }
   
+    override func numberOfSections(in tableView: UITableView) -> Int {
+      return self.workouts.count
+    }
+  
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-      return workouts.count
+      return self.workouts[section].count
+    }
+  
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+      return self.sectionTitles[section]
     }
   
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
       let cell = tableView.dequeueReusableCell(withIdentifier: "workoutCell", for: indexPath) as! WorkoutTableViewCell
-      let workout = workouts[indexPath.row]
+      let workout = workouts[indexPath.section][indexPath.row]
       cell.title?.text = workout.title!
       cell.location?.text = workout.location!
       cell.time?.text = workout.time!
@@ -61,13 +69,24 @@ class WorkoutsTableViewController: UITableViewController {
       performSegue(withIdentifier: "toShowPage", sender: indexPath)
     }
   
+    // Function to parse through the workouts and return the [Workout] object
+    func populateWorkouts(swiftyjson: JSON) -> [Workout] {
+      var workouts = [Workout]()
+      for json in swiftyjson.arrayValue {
+        // Since Workout model expects to parse a json with key "workout", setting it here
+        let workout = Workout(swiftyjson: ["workout": json])
+        workouts.append(workout)
+      }
+      return workouts
+    }
+  
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
       if (segue.identifier == "toShowPage") {
         if let destNavController = segue.destination as? UINavigationController{
           if let destVC = destNavController.topViewController as? WorkoutShowViewController{
             let index = sender as! IndexPath
             // Fetch the id for that specific workout
-            destVC.workout_id = self.workouts[index.row].id
+            destVC.workout_id = self.workouts[index.section][index.row].id
           }
         }
       }
